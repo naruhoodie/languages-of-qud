0651e28f4dbbb889d38c6595a741ba8f08e0c69e
naruhoodie <naruhoodie@gmail.com>
2025-05-19 22:46:12 -0600

    ja Translator with appropriate linebreak rules

--- languages/lang-ja/Translator.ja.cs
+++ languages/lang-ja/Translator.ja.cs
@@ -6,9 +6,20 @@ namespace XRL.Language
     [Translator.LanguageProvider("ja")]
     public class TranslatorJapanese : TranslatorBase
     {
-        // Don't break after color control codes.
-        private char[] DontBreakAfter = { '&', '^' };
-        private char[] DontBreakBefore = { '。', '、' };
+        private char[] DontBreakAfter = {
+            // Don't break after color control codes.
+            '&', '^',
+            // starting punctuation
+            '(', '[', '｛', '〔', '〈', '《', '「', '『', '【', '〘', '〖', '〝', '\'', '"', '｟', '«' };
+        private char[] DontBreakBefore = {
+            // small kana and other modifiers
+            'ァ', 'ィ', 'ゥ', 'ェ', 'ォ', 'ッ', 'ャ', 'ュ', 'ョ', 'ヮ', 'ヵ', 'ヶ', 'ぁ', 'ぃ', 'ぅ', 'ぇ', 'ぉ', 'っ', 'ゃ', 'ゅ', 'ょ', 'ゎ', 'ゕ', 'ゖ', 'ㇰ', 'ㇱ', 'ㇲ', 'ㇳ', 'ㇴ', 'ㇵ', 'ㇶ', 'ㇷ', 'ㇸ', 'ㇹ', 'ㇺ', 'ㇻ', 'ㇼ', 'ㇽ', 'ㇾ', 'ㇿ', '々', '〻', 'ヽ', 'ヾ', 'ー', '゙', '゚',
+            // closing brackets/quotes
+            ')', ']', '｝', '〕', '〉', '》', '」', '』', '】', '〙', '〗', '〟', '\'', '"', '｠', '»',
+            // hyphens
+            '‐', '゠', '–', '〜',
+            // delimiters and non-starting sentence punctuation
+            '？', '!', '‼', '⁇', '⁈', '⁉', '・', '、', ':', ';', ',', '.', '。', '、' };
         private bool CanBreakBefore(char @char)
         {
             if (@char == ' ' || @char == '\n') return true;
@@ -24,12 +35,13 @@ namespace XRL.Language
                 BreakBeforeIndex++;
             }
             // keep the ruby blob together
-            if (Span[BreakBeforeIndex-1] == '[')
+            if (Span[BreakBeforeIndex - 1] == '[')
             {
                 while (BreakBeforeIndex < Span.Length && Span[BreakBeforeIndex] != ']')
                 {
                     BreakBeforeIndex++;
                 }
+                BreakBeforeIndex++;
             }
             ReplaceIfBroken = BreakBeforeIndex < Span.Length && (Span[BreakBeforeIndex] == ' ' || Span[BreakBeforeIndex] == '\n');
         }
