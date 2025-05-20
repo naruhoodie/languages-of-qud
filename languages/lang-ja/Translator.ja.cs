e8f4f40bf26d8d66e2c36b2ad4ba15d29d08c279
naruhoodie <naruhoodie@gmail.com>
2025-05-20 03:20:57 -0600

    linebreak checking for surrogate pairs and non-spacing/combining marks

--- languages/lang-ja/Translator.ja.cs
+++ languages/lang-ja/Translator.ja.cs
@@ -1,32 +1,55 @@
 using System;
 using XRL;
+using System.Globalization;
 
 namespace XRL.Language
 {
     [Translator.LanguageProvider("ja")]
     public class TranslatorJapanese : TranslatorBase
     {
+        private const char  HIGH_SURROGATE_START  = '\ud800';
+        private const char  HIGH_SURROGATE_END    = '\udbff';
+        private const char  LOW_SURROGATE_START   = '\udc00';
+        private const char  LOW_SURROGATE_END     = '\udfff';
+
         private char[] DontBreakAfter = {
             // Don't break after color control codes.
             '&', '^',
-            // starting punctuation
-            '(', '[', '｛', '〔', '〈', '《', '「', '『', '【', '〘', '〖', '〝', '\'', '"', '｟', '«' };
+            // or simple quotes
+            '\'', '"'
+        };
         private char[] DontBreakBefore = {
             // small kana and other modifiers
-            'ァ', 'ィ', 'ゥ', 'ェ', 'ォ', 'ッ', 'ャ', 'ュ', 'ョ', 'ヮ', 'ヵ', 'ヶ', 'ぁ', 'ぃ', 'ぅ', 'ぇ', 'ぉ', 'っ', 'ゃ', 'ゅ', 'ょ', 'ゎ', 'ゕ', 'ゖ', 'ㇰ', 'ㇱ', 'ㇲ', 'ㇳ', 'ㇴ', 'ㇵ', 'ㇶ', 'ㇷ', 'ㇸ', 'ㇹ', 'ㇺ', 'ㇻ', 'ㇼ', 'ㇽ', 'ㇾ', 'ㇿ', '々', '〻', 'ヽ', 'ヾ', 'ー', '゙', '゚',
-            // closing brackets/quotes
-            ')', ']', '｝', '〕', '〉', '》', '」', '』', '】', '〙', '〗', '〟', '\'', '"', '｠', '»',
+            'ァ', 'ィ', 'ゥ', 'ェ', 'ォ', 'ッ', 'ャ', 'ュ', 'ョ', 'ヮ', 'ヵ', 'ヶ', 'ぁ', 'ぃ', 'ぅ', 'ぇ', 'ぉ', 'っ', 'ゃ', 'ゅ', 'ょ', 'ゎ', 'ゕ', 'ゖ', 'ㇰ', 'ㇱ', 'ㇲ', 'ㇳ', 'ㇴ', 'ㇵ', 'ㇶ', 'ㇷ', 'ㇸ', 'ㇹ', 'ㇺ', 'ㇻ', 'ㇼ', 'ㇽ', 'ㇾ', 'ㇿ', '々', '〻', 'ヽ', 'ヾ', 'ー', '゛', '゜', 'ゝ', 'ゞ',
             // hyphens
             '‐', '゠', '–', '〜',
             // delimiters and non-starting sentence punctuation
-            '？', '!', '‼', '⁇', '⁈', '⁉', '・', '、', ':', ';', ',', '.', '。', '、' };
+            '？', '!', '‼', '⁇', '⁈', '⁉', '・', '、', ':', ';', ',', '.', '。', '、',
+            // simple quotes
+            '\'', '"'
+        };
         private bool CanBreakBefore(char @char)
         {
             if (@char == ' ' || @char == '\n') return true;
             if (@char <= 255) return false;
+            if (@char >= LOW_SURROGATE_START && @char <= LOW_SURROGATE_END)
+                return false;
+            var uc = CharUnicodeInfo.GetUnicodeCategory(@char);
+            if (uc == UnicodeCategory.NonSpacingMark ||
+                uc == UnicodeCategory.SpacingCombiningMark ||
+                uc == UnicodeCategory.EnclosingMark ||
+                uc == UnicodeCategory.ClosePunctuation)
+                return false;
             return !DontBreakBefore.Contains(@char);
         }
-        private bool CanBreakAfter(char @char) => !DontBreakAfter.Contains(@char);
+        private bool CanBreakAfter(char @char)
+        {
+            if (@char >= HIGH_SURROGATE_START && @char <= HIGH_SURROGATE_END)
+                return false;
+            if (CharUnicodeInfo.GetUnicodeCategory(@char) == UnicodeCategory.OpenPunctuation) return false;
+            return !DontBreakAfter.Contains(@char);
+        }
+
         public override void NextPossibleLineBreakIndex(ReadOnlySpan<char> Span, int StartIndex, out int BreakBeforeIndex, out bool ReplaceIfBroken)
         {
             BreakBeforeIndex = StartIndex + 1;
