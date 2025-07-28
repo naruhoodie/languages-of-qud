770e26722122b43c4f5d0e99640e582caf6fd48f
naruhoodie <naruhoodie@gmail.com>
2025-07-28 14:32:04 -0600

    simplified Cardinal implementation

--- languages/lang-ja/Code/Translator.ja.cs
+++ languages/lang-ja/Code/Translator.ja.cs
@@ -82,35 +82,13 @@ namespace XRL.Language
         /**
          * support method for Cardinal() and Ordinal()
          */
-        private static void ProcessMagnitude(ref int num, ref int magnitude, TextBuilder result, string place)
-        {
-            if (magnitude > 4)
-            {
-                magnitude -= magnitude % 4;
-            }
-            int offset = (int)Math.Floor(Math.Exp(magnitude * Math.Log(10)));
-            int remainder = num % offset;
-            int val = num - remainder;
-            int count = val / offset;
-            if (count > 0)
-            {
-                result.Append(Cardinal(count));
-                result.Append(place);
-                num = remainder;
-            }
-            magnitude--;
-        }
-
-        /**
-         * support method for Cardinal() and Ordinal()
-         */
         private static void ProcessMagnitude(ref long num, ref int magnitude, TextBuilder result, string place)
         {
             if (magnitude > 4)
             {
                 magnitude -= magnitude % 4;
             }
-            int offset = (int)Math.Floor(Math.Exp(magnitude * Math.Log(10)));
+            long offset = (long)Math.Floor(Math.Exp(magnitude * Math.Log(10)));
             long remainder = num % offset;
             long val = num - remainder;
             long count = val / offset;
@@ -123,119 +101,6 @@ namespace XRL.Language
             magnitude--;
         }
 
-        /**
-         * support method for Cardinal() and Ordinal()
-         */
-        private static bool ProcessMagnitudes(ref int num, ref int magnitude, TextBuilder result, string suffix = null)
-        {
-            switch (magnitude)
-            {
-                case 20:
-                    ProcessMagnitude(ref num, ref magnitude, result, "垓");
-                    if (num == 0)
-                    {
-                        if (suffix != null)
-                        {
-                            result.Append(suffix);
-                        }
-                        return true;
-                    }
-                    goto case 16;
-                case 19:
-                case 18:
-                case 17:
-                case 16:
-                    ProcessMagnitude(ref num, ref magnitude, result, "京");
-                    if (num == 0)
-                    {
-                        if (suffix != null)
-                        {
-                            result.Append(suffix);
-                        }
-                        return true;
-                    }
-                    goto case 12;
-                case 15:
-                case 14:
-                case 13:
-                case 12:
-                    ProcessMagnitude(ref num, ref magnitude, result, "兆");
-                    if (num == 0)
-                    {
-                        if (suffix != null)
-                        {
-                            result.Append(suffix);
-                        }
-                        return true;
-                    }
-                    goto case 8;
-                case 11:
-                case 10:
-                case 9:
-                case 8:
-                    ProcessMagnitude(ref num, ref magnitude, result, "億");
-                    if (num == 0)
-                    {
-                        if (suffix != null)
-                        {
-                            result.Append(suffix);
-                        }
-                        return true;
-                    }
-                    goto case 4;
-                case 7:
-                case 6:
-                case 5:
-                case 4:
-                    ProcessMagnitude(ref num, ref magnitude, result, "万");
-                    if (num == 0)
-                    {
-                        if (suffix != null)
-                        {
-                            result.Append(suffix);
-                        }
-                        return true;
-                    }
-                    goto case 3;
-                case 3:
-                    ProcessMagnitude(ref num, ref magnitude, result, "千");
-                    if (num == 0)
-                    {
-                        if (suffix != null)
-                        {
-                            result.Append(suffix);
-                        }
-                        return true;
-                    }
-                    goto case 2;
-                case 2:
-                    ProcessMagnitude(ref num, ref magnitude, result, "百");
-                    if (num == 0)
-                    {
-                        if (suffix != null)
-                        {
-                            result.Append(suffix);
-                        }
-                        return true;
-                    }
-                    goto case 1;
-                case 1:
-                    if (magnitude > 0)
-                    {
-                        ProcessMagnitude(ref num, ref magnitude, result, "十");
-                        if (num == 0)
-                        {
-                            if (suffix != null)
-                            {
-                                result.Append(suffix);
-                            }
-                            return true;
-                        }
-                    }
-                    break;
-            }
-            return false;
-        }
         /**
          * support method for Cardinal() and Ordinal()
          */
@@ -356,27 +221,7 @@ namespace XRL.Language
             {
                 return "零";
             }
-            using var result = TextBuilder.Get();
-            if (num < 0)
-            {
-                result.Append("マイナス");
-                num = -num;
-            }
-            int magnitude = (int)Math.Floor(Math.Log10(num));
-            ProcessMagnitudes(ref num, ref magnitude, result);
-            switch (num)
-            {
-                case 1: result.Append("一"); break;
-                case 2: result.Append("二"); break;
-                case 3: result.Append("三"); break;
-                case 4: result.Append("四"); break;
-                case 5: result.Append("五"); break;
-                case 6: result.Append("六"); break;
-                case 7: result.Append("七"); break;
-                case 8: result.Append("八"); break;
-                case 9: result.Append("九"); break;
-            }
-            return result.ToString();
+            return Cardinal((long) num);
         }
         public static string Cardinal(long num)
         {
